f1efcd3fbb95957143fc58b8b5ed4f47ecf83b7c
Your Name <ozlemhayalkurun@gmail.com>
2022-10-03 15:30:55 +0300

    add searchBar
    add searchBar

--- musicProject/src/App.js
+++ musicProject/src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import {Text,View,StyleSheet,FlatList} from 'react-native';
 import musicData from './music_app.json';
 import SongCard from "./components/SongCard/SongCard";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App(){
+  const [list, setList] = useState(musicData);
+
   const renderSong = ({item}) => <SongCard song={item}/>
   const renderSeparator = () => <View style={styles.separator} />
+
+  const handleSearch = text => {
+    const filteredList = musicData.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+
+    setList(filteredList);
+  };
+
+
   return(
     <View style={styles.container}>
+      <SearchBar onSearch={handleSearch} />
       <FlatList
         keyExtractor={item => item.id}
-        data={musicData}
+        data={list}
         renderItem={renderSong}
         ItemSeparatorComponent={renderSeparator}
       />
---
+++ musicProject/src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor:'#cccccc',
+        borderRadius:10,
+        padding:1,
+        margin:5
+    }
+});
